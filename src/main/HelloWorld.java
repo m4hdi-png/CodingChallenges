da411cae553bfb815b3a6802c7f3f8b85e5413b9
Matthew <mattmatthew9977@gmail.com>
2025-01-18 21:36:26 -0800

    second run after adding java and test file

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+     
+    public static void main(String[] args) { // function testing this class if you dont have teh test code 
+        System.err.println("Helloworks");
+    }
+}
